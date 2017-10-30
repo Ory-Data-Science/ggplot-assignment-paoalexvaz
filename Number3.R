d62e5db761734c28d02dafd355be3c7577eacbb2
Paola Vazquez <paola.vazquez@stlcop.edu>
2017-10-30 14:05:45 -0500

    Revert "Number2.R"
    This reverts commit afdb907a46b860eed93d972f927e280a295b9061.

--- Number3.R
+++ Number3.R
@@ -6,39 +6,3 @@ dat <- read.csv(url("http://esapubs.org/archive/ecol/E084/093/Mammal_lifehistori
                 sep = "\t", nrows = 1440, na.strings = c("-999.00", "999.00"))
 
 #rest of your code goes here
-#Plot 1
-ggplot(data = dat, aes(x = mass.g., y = newborn.g.)) +
-  labs(x = "Adult Mass", y = "Newborn Mass") +
-  geom_point()
-
-#Plot 2
-ggplot(data = dat, aes(x = mass.g., y = newborn.g.)) +
-  labs(x = "Adult Mass", y = "Newborn Mass") +
-  scale_x_log10() +
-  scale_y_log10() +
-  geom_point()
-
-#Plot 3
-ggplot(data = dat, aes(x = mass.g., y = newborn.g., color = order)) +
-  labs(x = "Adult Mass", y = "Newborn Mass") +
-  scale_x_log10() +
-  scale_y_log10() +
-  geom_point()
-
-#Plot 4
-ggplot(data = dat, aes(x = mass.g., y = newborn.g.)) +
-  labs(x = "Adult Mass", y = "Newborn Mass") +
-  scale_x_log10() +
-  scale_y_log10() +
-  geom_point() +
-  facet_wrap(~ order, nrow = 4, ncol = 5)
-
-#Plot 5
-ggplot(data = dat, aes(x = mass.g., y = newborn.g.)) +
-  labs(x = "Adult Mass", y = "Newborn Mass") +
-  scale_x_log10() +
-  scale_y_log10() +
-  geom_point() +
-  geom_smooth(method = "lm") +
-  facet_wrap(~ order, nrow = 4, ncol = 5)
-
